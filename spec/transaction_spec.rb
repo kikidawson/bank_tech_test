b3583c718dd8bfc81bb93b6859267889021e8e06
Kiki Dawson <kikiadawson@gmail.com>
2021-02-08 16:50:56 +0000

    Create Transaction class

---
+++ spec/transaction_spec.rb
@@ -0,0 +1,25 @@
+require 'transaction'
+
+describe Transaction do
+  subject { described_class.new('08/02/2021', 1000, 0, 1000) }
+
+  it 'allows instances of a class to be created' do
+    expect(subject).to be_kind_of Transaction
+  end
+
+  it 'can return the transaction date' do
+    expect(subject.date).to eq '08/02/2021'
+  end
+
+  it 'can return how much is being credited' do
+    expect(subject.credit).to eq 1000
+  end
+
+  it 'can return how much is being debited' do
+    expect(subject.debit).to eq 0
+  end
+
+  it 'can return the balance' do
+    expect(subject.balance).to eq 1000
+  end
+end
