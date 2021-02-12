6d9152322d7be0dd59b15e84fb90df983c0d5848
Kiki Dawson <kikiadawson@gmail.com>
2021-02-12 15:09:44 +0000

    Add stringify transactions emthod

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,23 +2,23 @@
 
 class Statement
   def initialize(transactions)
-    # transaction = [ one, two, three ]
     @transactions = transactions
-    @transactions_stringify = [("date || credit || debit || balance")]
   end
 
   def print
+    puts "date || credit || debit || balance"
+    stringify_transactions
     @transactions.reverse_each do |transaction|
-      @transactions_stringify << "#{transaction.date} || #{convert(transaction.credit)} || #{convert(transaction.debit)} || #{convert(transaction.balance)}"
-    end
-    @transactions_stringify.each do |transaction|
       puts transaction
     end
   end
 
   private
 
-  def print_each(transaction)
+  def stringify_transactions
+    @transactions.map! do |transaction|
+      "#{transaction.date} || #{convert(transaction.credit)} || #{convert(transaction.debit)} || #{convert(transaction.balance)}"
+    end
   end
 
   def convert(value)
