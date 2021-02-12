f5d15a5012143c01d333c792edbcb7f00591e0f1
Kiki Dawson <kikiadawson@gmail.com>
2021-02-12 15:06:16 +0000

    Refactor statement print method

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,17 +2,24 @@
 
 class Statement
   def initialize(transactions)
+    # transaction = [ one, two, three ]
     @transactions = transactions
+    @transactions_stringify = [("date || credit || debit || balance")]
   end
 
   def print
-    puts "date || credit || debit || balance\n"
     @transactions.reverse_each do |transaction|
-      puts "#{transaction.date} || #{convert(transaction.credit)} || #{convert(transaction.debit)} || #{convert(transaction.balance)}\n"
+      @transactions_stringify << "#{transaction.date} || #{convert(transaction.credit)} || #{convert(transaction.debit)} || #{convert(transaction.balance)}"
+    end
+    @transactions_stringify.each do |transaction|
+      puts transaction
     end
   end
 
   private
+
+  def print_each(transaction)
+  end
 
   def convert(value)
     format('%.2f', value) if value.is_a? Integer
